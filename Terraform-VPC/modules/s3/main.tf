8a49c2d50930be82ffe7dc63b36aae83a2a5e68a
Parashara <parasharar@gmail.com>
2024-03-18 11:43:43 +0800

    fixing bucket name for s3 bucket for elastic bean stalk

--- Terraform-VPC/modules/s3/main.tf
+++ Terraform-VPC/modules/s3/main.tf
@@ -1,6 +1,6 @@
 # s3 bucket for storing Elastic Bean stalk code builds
-resource "aws_s3_bucket" "split_elastic_beanstalk_deploy" {
-  bucket = "split_elastic_beanstalk_deploy"
+resource "aws_s3_bucket" "split-elastic-beanstalk-deploy-bucket" {
+  bucket = "split-elastic-beanstalk-deploy-bucket"
 
   tags = {
     Name        = "Split a bill Elastic beanstalk bucket"
